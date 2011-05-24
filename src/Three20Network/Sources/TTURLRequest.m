773b00c5c7a103155b436822c2a6ca79e22c2c39
Rafael Alba <ralba@socialcast.com>
2011-05-24 14:17:54 -0700

    applied the custom parameter name for file uploads patch to three 20

--- src/Three20Network/Sources/TTURLRequest.m
+++ src/Three20Network/Sources/TTURLRequest.m
@@ -179,9 +179,6 @@ static NSString* kStringBoundary = @"3i2ndDfv2rTHiSisAbouNdArYfORhtTPEefj3q2f";
   NSMutableData* body = [NSMutableData data];
   NSString* beginLine = [NSString stringWithFormat:@"\r\n--%@\r\n", kStringBoundary];
 
-  [body appendData:[[NSString stringWithFormat:@"--%@\r\n", kStringBoundary]
-    dataUsingEncoding:NSUTF8StringEncoding]];
-
   for (id key in [_parameters keyEnumerator]) {
     NSString* value = [_parameters valueForKey:key];
     // Really, this can only be an NSString. We're cheating here.
@@ -212,15 +209,16 @@ static NSString* kStringBoundary = @"3i2ndDfv2rTHiSisAbouNdArYfORhtTPEefj3q2f";
     }
   }
 
-  for (NSInteger i = 0; i < _files.count; i += 3) {
+  for (NSInteger i = 0; i < _files.count; i += 4) {
     NSData* data = [_files objectAtIndex:i];
-    NSString* mimeType = [_files objectAtIndex:i+1];
-    NSString* fileName = [_files objectAtIndex:i+2];
+    NSString* name = [_files objectAtIndex:i+1];
+    NSString* mimeType = [_files objectAtIndex:i+2];
+    NSString* fileName = [_files objectAtIndex:i+3];
 
     [body appendData:[beginLine dataUsingEncoding:NSUTF8StringEncoding]];
     [body appendData:[[NSString stringWithFormat:
                        @"Content-Disposition: form-data; name=\"%@\"; filename=\"%@\"\r\n",
-                       fileName, fileName]
+                       name, fileName]
           dataUsingEncoding:_charsetForMultipart]];
     [body appendData:[[NSString stringWithFormat:@"Content-Length: %d\r\n", data.length]
           dataUsingEncoding:_charsetForMultipart]];
@@ -302,11 +300,18 @@ static NSString* kStringBoundary = @"3i2ndDfv2rTHiSisAbouNdArYfORhtTPEefj3q2f";
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 - (void)addFile:(NSData*)data mimeType:(NSString*)mimeType fileName:(NSString*)fileName {
+  [self addFile:data name:fileName mimeType:mimeType fileName:fileName];
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+- (void)addFile:(NSData*)data name:(NSString*)name mimeType:(NSString*)mimeType
+       fileName:(NSString*)fileName {
   if (!_files) {
     _files = [[NSMutableArray alloc] init];
   }
 
   [_files addObject:data];
+  [_files addObject:name];
   [_files addObject:mimeType];
   [_files addObject:fileName];
 }
